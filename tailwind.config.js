c1b3653a0eb74bd55e75b516f5b4c135d9d82722
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-10-29 19:43:12 +0200

    added some colors and screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(13, 100%, 72%)',
+        brightRedSupLight: 'hsl(353, 100%, 62%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 47%, 7%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+    },
   },
   plugins: [],
 };
